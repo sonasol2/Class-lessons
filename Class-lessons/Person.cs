9481782900b8ae3692ff2f7c799369e14471cee7
sonasol2 <57984322+sonasol2@users.noreply.github.com>
2024-02-02 17:29:59 +0200

    lesson done

---
+++ Class-lessons/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace Class_lessons
+{
+	//public class Person
+	//{
+		
+ //       public string name = "Undef";
+ //       public int age;
+ //       public Person() : this("undef") { }
+ //       public Person(string name) : this(name, 18) { }
+ //       public Person(string name, int age) { this.name = name; this.age = age; } //ключевое слово this здесь используется для того чтобы разграничить 
+ //       // поле класса(name и age) и название параметра(name и age). короче чтобы просто было понятнее что к чему относится. Используется в том случае когда названия полей и параметров конструктора одинаковое, если разное то можно не использорвать.
+
+ //       // также с помощь : this можно передавать значения для параметров к другому конструктору. Если к примеру при создании экземпляра
+ //       // использовался второй конустрктор, то второй обратится за значениями к третьему. Причем сначала выполнится третий конустркутор и потом уже второй.
+
+ //       // лучше всего использовать этот конструктор он более полноценный и сразу включает в сеюя дефолтные п: public Person(string name = "undef", int age = 18) {this.name = name; this.age = age;}
+
+ //       public void PrintInfo()
+ //       {
+ //           Console.WriteLine($"Your name:{name}Your age is: {age}");
+ //       }
+ //   }
+	
+		class Person
+	{
+		string name;
+		int age;
+		public Person(string name, int age)
+		{
+			this.name = name;
+			this.age = age; 
+		} 
+
+		public void Deconstruct(out string personName, out int personAge)
+		{
+			personName = name;
+			personAge = age; 
+		}
+    }
+}
+
--- Class-lessons/Program.cs
+++ Class-lessons/Program.cs
@@ -1,10 +1,27 @@
 ﻿namespace Class_lessons;
+using Class_lessons;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+
+        Person jack = new Person(); //  конструктор 
+        Person fred = new Person("Fred", 42);
+        Person someone = new Person("F42");
+
+        string personName = jack.name;
+        int personAge = jack.age;
+        //Console.WriteLine($"Name is {personName} Age is {personAge}");
+
+        //jack.name = "Jack";
+        //jack.age = 34;
+
+
+        jack.PrintInfo();
+        fred.PrintInfo();
+        someone.PrintInfo();
     }
 }
+
 
